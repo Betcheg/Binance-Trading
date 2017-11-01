25042dfb18a25360f0e74d4c5505f045bb20a4d4
Betcheg <betcheg+github@gmail.com>
2017-11-01 00:29:26 -0400

    ajout serv

--- index.js
+++ index.js
@@ -16,12 +16,30 @@ function start() {
 }
 
 
+// Binance API dependencies
 const binance = require('node-binance-api');
 const binanceAPIkey = require('../secret_key/binance-key');
+
+// Websocket dependencies
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
 
 // Set the API key 
 binanceAPIkey.setAPI(binance);
 
+app.use(express.static(__dirname + '/src'));
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/src/html/index.html');
+});
+
+
+// Start the web server
+http.listen(5555, function(){
+  console.log('listening on *:3000');
+});
+    
 // Start the bot
 setInterval(start, 1000);
 
