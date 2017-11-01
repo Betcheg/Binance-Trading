1606893c1e95f3d71bd165d3b05b77c7b67c7964
Betcheg <betcheg+github@gmail.com>
2017-11-01 01:23:10 -0400

    add socket

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ function start() {
 
         binance.prices(function(ticker) {
             console.log("Price of WTC: ", ticker.WTCBTC);
+            io.emit('price wtc', ticker.WTCBTC); 
         });
     
 
     binance.balance(function(balances) {
 	if ( typeof balances.WTC !== "undefined" ) {
 		console.log("WTC balance: ", balances.WTC.available);
+            io.emit('balance wtc', balances.WTC.available); 
 	}
 });
 
@@ -24,11 +26,13 @@ const binanceAPIkey = require('../secret_key/binance-key');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 
 // Set the API key 
 binanceAPIkey.setAPI(binance);
 
+// Set the html routing
 app.use(express.static(__dirname + '/src'));
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/src/html/index.html');
@@ -39,8 +43,22 @@ app.get('/', function(req, res){
 http.listen(5555, function(){
   console.log('listening on *:3000');
 });
+
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+ 
+    io.emit('connection ok', "aaa"); 
     
+    socket.on('disconnect', function(){
+    console.log('user disconnected');
+    });
+});
+    
+
 // Start the bot
 setInterval(start, 1000);
+
+
 
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,55 @@
 $(document).ready(function(){
+    var socket = io();
+    var lastWTCPrice = 0;
+    var balanceWTC = 0;
 
-        salut();
+    socket.on('connection ok', function(msg){
+        $('.content').prepend("<i> Successfully connected</i><br>");
+        $('.titre').html("State: <b> Running </b>");
+        console.log("OK: "+msg);
+    });
 
-        $.ajax({url: "https://www.binance.com/api/v1/ping", success: function(result){
-            $(".content").html(result);
-        }});
+    socket.on('disconnect', function(msg){
+        $('.content').prepend("<i> Disconnected </i><br>");
+        $('.titre').html("State: <span class='red'> <b>Disconnected</b> </span>");
+    });
+
+
+
+    socket.on('price wtc', function(msg){
+        console.log("price: "+msg);
+        var currentWTCprice = parseFloat(msg);
+        var spanColor = "<span color='blue'>";
+        if(lastWTCPrice < currentWTCprice) {
+            spanColor = "<span class='green'>";
+            lastWTCPrice = currentWTCprice;
+            $('.content').prepend( getTime() + "WTC price" + spanColor+ " "+  msg + "</span><br>");
+        }
+        else if(lastWTCPrice > currentWTCprice) {
+            spanColor = "<span class='red'>";
+            lastWTCPrice = currentWTCprice;
+            $('.content').prepend(getTime() + "WTC price " +spanColor + " "+  msg + "</span><br>");
+        }
+
+
+        $("#price #wtc").html(" " + currentWTCprice +" WTC ");
+
+    });
+
+
+    socket.on('balance wtc', function(msg){
+        imgURL = "<img src='https://files.coinmarketcap.com/static/img/coins/16x16/walton.png'/>" ;
+        balanceWTC = parseFloat(msg);
+        $("#holding #wtc").html(balanceWTC + " WTC " + imgURL);
+    });
+
+
+    function getTime(){
+
+        var d = new Date();
+        return "["+d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + "] ";
+
+    }
 
 });
 
-function salut() {
-    console.log("salut");
-}
